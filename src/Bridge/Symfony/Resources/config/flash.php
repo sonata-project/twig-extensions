36bb01ad19a46114a4e30d25d9c334917c49c968
Fran Moreno <franmomu@gmail.com>
2020-09-09 14:02:12 +0200

    Move internal configuration to PHP

---
+++ src/Bridge/Symfony/Resources/config/flash.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Sonata Project package.
+ *
+ * (c) Thomas Rabaix <thomas.rabaix@sonata-project.org>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Sonata\Twig\Extension\FlashMessageExtension;
+use Sonata\Twig\Extension\FlashMessageRuntime;
+use Sonata\Twig\FlashMessage\FlashManager;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ReferenceConfigurator;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->parameters()
+
+        ->set('sonata.twig.flashmessage.manager.class', FlashManager::class)
+
+        ->set('sonata.twig.extension.flashmessage.class', FlashMessageExtension::class)
+    ;
+
+    // Use "service" function for creating references to services when dropping support for Symfony 4.4
+    // Use "param" function for creating references to parameters when dropping support for Symfony 5.1
+    $containerConfigurator->services()
+
+        ->set('sonata.twig.flashmessage.manager', '%sonata.twig.flashmessage.manager.class%')
+            ->public()
+            ->tag('sonata.status.renderer')
+            ->args([
+                new ReferenceConfigurator('session'),
+                [],
+                [],
+            ])
+
+        ->set('sonata.twig.flashmessage.twig.runtime', FlashMessageRuntime::class)
+            ->tag('twig.runtime')
+            ->args([
+                new ReferenceConfigurator('sonata.twig.flashmessage.manager'),
+            ])
+
+        ->set('sonata.twig.flashmessage.twig.extension', '%sonata.twig.extension.flashmessage.class%')
+            ->public()
+            ->tag('twig.extension')
+    ;
+};
