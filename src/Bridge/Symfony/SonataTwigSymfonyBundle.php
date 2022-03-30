8d62e1d0f54b747d201d9daf2a28ebc117a8b00c
Jordi Sala Morales <jordism91@gmail.com>
2022-03-30 10:15:18 +0200

    Deprecate custom flex bundle file (#316)

--- src/Bridge/Symfony/SonataTwigSymfonyBundle.php
+++ src/Bridge/Symfony/SonataTwigSymfonyBundle.php
@@ -13,8 +13,10 @@ declare(strict_types=1);
 
 namespace Sonata\Twig\Bridge\Symfony;
 
-// This file and its class alias is required in order to let Symfony Flex
-// autodiscovery to find the bundle.
-// The string "Symfony\Component\HttpKernel\Bundle\Bundle" must also be present.
-// @see https://github.com/symfony/flex/pull/612/files.
+@trigger_error(sprintf(
+    'The %s\SonataTwigSymfonyBundle class is deprecated since sonata-project/twig-extensions 1.x, to be removed in version 2.0. Use %s instead.',
+    __NAMESPACE__,
+    SonataTwigBundle::class
+), \E_USER_DEPRECATED);
+
 class_alias(SonataTwigBundle::class, __NAMESPACE__.'\SonataTwigSymfonyBundle');
