c0591ad937d1ef390dbcb472f27ebd4fe2d0b041
Vincent Langlet <VincentLanglet@users.noreply.github.com>
2022-06-14 23:11:45 +0200

    Fix cs (#332)

--- src/Extension/FormTypeExtension.php
+++ src/Extension/FormTypeExtension.php
@@ -20,7 +20,7 @@ final class FormTypeExtension extends AbstractExtension implements GlobalsInterf
 {
     private bool $wrapFieldsWithAddons;
 
-    public function __construct(bool | string $formType)
+    public function __construct(bool|string $formType)
     {
         $this->wrapFieldsWithAddons = (true === $formType || 'standard' === $formType);
     }
